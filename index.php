4eadfd6e2ca964f67793f4ab5a0107126bee55e2
u3206769 <alex.catherine@live.com>
2021-08-02 11:33:04 +1000

    commit message

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+<title>Page title</title>
+<meta charset="utf-8">
+</head>
+<body>
+    <?php for ($i=0; $i<50; $i++){
+        echo "<p>Hello world</p>"; 
+    }
+    //declaring var $// ?>
+
+</body>
+</html>
+
